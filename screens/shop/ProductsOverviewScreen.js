9ffa92bb2810cf689b819f0655194cdceeb3e01c
Madhav Khurana <madhavkhurana086@gmail.com>
2019-09-26 00:24:43 -0500

    Added main and product screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { FlatList, Text } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import Productitem from "../../components/shop/ProductItem";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
   return (
     <FlatList
       data={products}
-      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+      renderItem={itemData => (
+        <Productitem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            });
+          }}
+          onAddToCart={() => {
+            dispatch(cartActions.AddToCart(itemData.item));
+          }}
+        />
+      )}
       keyExtractor={item => item.id}
     />
   );
